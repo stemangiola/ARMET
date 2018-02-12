0458997e99a38bc1e9cf4316fe86390b73d618ef
stemangiola <mangiolastefano@gmail.com>
2018-02-12 15:08:03 +1100

    more custom parameters for testing

--- R/ARMET_tc.R
+++ R/ARMET_tc.R
@@ -20,25 +20,19 @@ ARMET_tc = function(
 	verbose =                           F,
 	sigma_hyper_sd =                    0.01,
 	phi_hyper_sd =                      5,
-	alpha_hyper_value =                 10
+	alpha_hyper_value =                 10,
+	save_report =                       F,
+	custom_ref =                        NULL
 ){
 	
 	#Read ini file for some options
-	if(file.exists("ARMET.ini")) {
-		pars =  ini:::read.ini("ARMET.ini")$ARMET
-		writeLines("Importing parameters from .ini file..")
-		for(n in names(pars)){
-			if(pars[n]%in%c("T", "F", "TRUE", "FALSE"))
-				pars[n] = as.logical(pars[n])
-			assign(n, unlist(pars[n]))
-		}
-	}
+	get_ini()
 	
 	# Check input
-	check_input(mix, is_mix_microarray, my_design, cov_to_test, sigma_hyper_sd)
+	check_input(mix, is_mix_microarray, my_design, cov_to_test, sigma_hyper_sd, custom_ref, tree)
 	
 	# Create directory
-	output_dir = create_temp_result_directory()
+	output_dir = if(save_report)        create_temp_result_directory() else NULL
 	
 	# Format input
 	mix =                               as.matrix(mix)
@@ -53,7 +47,8 @@ ARMET_tc = function(
 	# ref_array = as.matrix(read.csv("~/PhD/deconvolution/ARMET_dev/ARMET_TME_signature_df_array.csv", header=T, row.names=1))
 	# colnames(ref_array) = as.vector(sapply(colnames(ref_array), function(cn) strsplit(cn, ".", fixed=T)[[1]][1]))
 	# save(ref_array, file="data/ref_array.rda")
-	ref = if(!is_mix_microarray) ref_RNAseq else ref_array
+	if(!is.null(custom_ref))            ref = custom_ref 
+	else                                ref = if(!is_mix_microarray) ref_RNAseq else ref_array
 
 	
 	# Create trees
@@ -61,8 +56,8 @@ ARMET_tc = function(
 	# tree = read_json("/wehisan/home/allstaff/m/mangiola.s/PhD/deconvolution/ARMET_dev/ARMET_TME_tree_RNAseq.json")
 	# save(tree, file="data/tree_json.rda")
 	data(tree_json)
-	my_tree = drop_node_from_tree(tree, ct_to_omit)
-	ref = ref[, colnames(ref)%in%get_leave_label(my_tree, last_level = 0, label = "name")]
+	my_tree =                           drop_node_from_tree(tree, ct_to_omit)
+	ref =                               ref[, colnames(ref)%in%get_leave_label(my_tree, last_level = 0, label = "name")]
 	
 	# Make data sets comparable
 	common_genes =                      intersect(rownames(mix), rownames(ref))
@@ -75,7 +70,7 @@ ARMET_tc = function(
 	mix = 															norm.obj$mix
 	
 	# Save density plot
-	#ggplot2:::ggsave(sprintf("%s/densities.png", output_dir), plot=norm.obj$plot)
+	if(save_report) ggplot2:::ggsave(sprintf("%s/densities.png", output_dir), plot=norm.obj$plot)
 	
 	## Execute core ##############################################################################
 	##############################################################################################
@@ -83,19 +78,20 @@ ARMET_tc = function(
 	my_tree = run_coreAlg_though_tree(
 		my_tree, 
 		list(
-			mix =                      mix, 
-			ref =                      ref, 
-			my_design=                 my_design, 
-			cov_to_test =              cov_to_test, 
-			fully_bayesian =           fully_bayesian,
-			observed_prop =            observed_prop, 
-			ct_to_omit =               ct_to_omit, 
-			my_tree =                  my_tree, 
-			is_mix_microarray =        is_mix_microarray,
-			output_dir =               output_dir,
-			sigma_hyper_sd =           sigma_hyper_sd,
-			phi_hyper_sd =             phi_hyper_sd,
-			alpha_hyper_value =        alpha_hyper_value
+			mix =                           mix, 
+			ref =                           ref, 
+			my_design=                      my_design, 
+			cov_to_test =                   cov_to_test, 
+			fully_bayesian =                fully_bayesian,
+			observed_prop =                 observed_prop, 
+			ct_to_omit =                    ct_to_omit, 
+			my_tree =                       my_tree, 
+			is_mix_microarray =             is_mix_microarray,
+			save_report =                   save_report,
+			output_dir =                    output_dir,
+			sigma_hyper_sd =                sigma_hyper_sd,
+			phi_hyper_sd =                  phi_hyper_sd,
+			alpha_hyper_value =             alpha_hyper_value
 		)
 	)
 		
@@ -103,20 +99,20 @@ ARMET_tc = function(
 	##############################################################################################
 
 	# Create one tree per sample
-	my_trees =                          divide_trees_proportion_across_many_trees(my_tree)
+	my_trees =                            divide_trees_proportion_across_many_trees(my_tree)
 	
 	# Calculate absolute proportions
 	my_trees = 														add_absolute_proportions_to_trees(my_trees)
 	
 	# Produce table proportions
-	proportions = 											get_proportions_table(my_trees)
+	proportions = 											  get_proportions_table(my_trees)
 	
 	# Check if markers were reliable
-	markers = intersect(intersect(get_genes(tree), rownames(ref)), rownames(mix))
-	signatures = 												list()
-	signatures$orig = 									get_mean_signature(ref[markers,], verbose = F)
-	signatures$orig =                   signatures$orig[,colnames(signatures$orig)%in%colnames(proportions)]
-	signatures$predicted =							NULL
+	markers =                             intersect(intersect(get_genes(tree), rownames(ref)), rownames(mix))
+	signatures = 												  list()
+	signatures$orig = 									  get_mean_signature(ref[markers,], verbose = F)
+	signatures$orig =                     signatures$orig[,colnames(signatures$orig)%in%colnames(proportions)]
+	signatures$predicted =							  NULL
 	
 	# Create tree with hypothesis testing
 	if(!is.null(cov_to_test) & 0) {
@@ -127,9 +123,9 @@ ARMET_tc = function(
 	}
 	
 	return(list(
-		proportions =                     proportions,
-		signatures =                      signatures,
-		mix =                             mix[markers,,drop=F]
+		proportions =                       proportions,
+		signatures =                        signatures,
+		mix =                               mix[markers,,drop=F]
 	))
 	
 }
