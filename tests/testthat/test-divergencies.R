bf2a9640ea6bf4aec10fda7af5f8239c5a0f47d0
stemangiola <mangiolastefano@gmail.com>
2019-11-25 17:32:24 +1100

    update tests

--- tests/testthat/test-divergencies.R
+++ tests/testthat/test-divergencies.R
@@ -1,11 +1,10 @@
 context('Test if divergent')
 
-library(rlang)
 library(tidyverse)
 library(ARMET)
-library(data.tree)
-library(foreach)
-library(magrittr)
+# library(data.tree)
+# library(foreach)
+# library(magrittr)
 
 res = readRDS("/stornext/Home/data/allstaff/m/mangiola.s/PhD/deconvolution/ARMET/dev/feature_selection_fifth_iterative_run_fix_skylake_2/markers_17.RData")
 load("dev/mix_dirichlet_X.rda")
@@ -55,3 +54,14 @@ result_dirichlet_fix =
 		X = X,
 		cores = 10, levels = 3
 	)
+
+N52_ARMET_T = ARMET_tc(
+	mix = readRDS("dev/mix_N52.rds"),
+	full_bayesian  = T,
+	do_regression = T,
+	cores = 10,
+	levels = 3
+)
+save(N52_ARMET_T, file="dev/N52_ARMET_T.rda")
+
+level_to_plot_inferred_vs_observed(N52_ARMET_T, 1, S = 13)
