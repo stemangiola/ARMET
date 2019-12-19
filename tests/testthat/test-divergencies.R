9774d1f7740c0298fd2886385ae35e33a258d870
stemangiola <mangiolastefano@gmail.com>
2019-12-20 10:22:43 +1100

    update test script

--- tests/testthat/test-divergencies.R
+++ tests/testthat/test-divergencies.R
@@ -6,19 +6,21 @@ library(ARMET)
 # library(foreach)
 # library(magrittr)
 
-res = readRDS("/stornext/Home/data/allstaff/m/mangiola.s/PhD/deconvolution/ARMET/dev/feature_selection_fifth_iterative_run_fix_skylake_2/markers_17.RData")
+res = readRDS(
+	"/stornext/Home/data/allstaff/m/mangiola.s/PhD/deconvolution/ARMET/dev/feature_selection_fifth_iterative_run_fix_skylake_2/markers_17.RData"
+)
 load("dev/mix_dirichlet_X.rda")
 
 
-result =
-	ARMET_tc(
-		res$input$mix %>% slice(1),
-		iterations = 50,
-		sampling_iterations = 5,
-		n_markers = res$input$n_markers,
-		full_bayesian  = T,
-		cores = 2, levels = 2
-	)
+# result =
+# 	ARMET_tc(
+# 		res$input$mix %>% slice(1),
+# 		iterations = 50,
+# 		sampling_iterations = 5,
+# 		n_markers = res$input$n_markers,
+# 		full_bayesian  = T,
+# 		cores = 2, levels = 2
+# 	)
 
 result_fix =
 	ARMET_tc(
@@ -26,21 +28,22 @@ result_fix =
 		iterations = 50,
 		sampling_iterations = 5,
 		full_bayesian  = F,
-		cores = 2, levels = 3
+		cores = 2,
+		levels = 3
 	)
 
 
-result_dirichlet =
-	ARMET_tc(
-		read_csv("dev/mix_dirichlet.csv") %>%
-			rename(sample = run) %>%
-			mutate(`count mix` = `count mix` %>% as.integer) %>%
-			spread(symbol, `count mix`) %>%
-			mutate(sample = sprintf("s%s", sample))	,
-		n_markers = res$input$n_markers,
-		full_bayesian  = T, do_regression = T,X = X,
-		cores = 10, levels = 3
-	)
+# result_dirichlet =
+# 	ARMET_tc(
+# 		read_csv("dev/mix_dirichlet.csv") %>%
+# 			rename(sample = run) %>%
+# 			mutate(`count mix` = `count mix` %>% as.integer) %>%
+# 			spread(symbol, `count mix`) %>%
+# 			mutate(sample = sprintf("s%s", sample))	,
+# 		n_markers = res$input$n_markers,
+# 		full_bayesian  = T, do_regression = T,X = X,
+# 		cores = 10, levels = 3
+# 	)
 
 result_dirichlet_fix =
 	ARMET_tc(
@@ -50,18 +53,70 @@ result_dirichlet_fix =
 			spread(symbol, `count mix`) %>%
 			mutate(sample = sprintf("s%s", sample))	,
 		n_markers = res$input$n_markers,
-		full_bayesian  = F, do_regression = T,
+		full_bayesian  = F,
+		do_regression = T,
 		X = X,
-		cores = 10, levels = 3
+		cores = 10,
+		levels = 3
 	)
 
-N52_ARMET_T = ARMET_tc(
-	mix = readRDS("dev/mix_N52.rds"),
-	full_bayesian  = T,
-	do_regression = T,
-	cores = 10,
-	levels = 3
-)
-save(N52_ARMET_T, file="dev/N52_ARMET_T.rda")
+N52_ARMET_T =
+	ARMET_tc(
+		spread(
+			select(
+				filter(
+					readRDS("dev/N52.rds"),
+					 ct == "T"
+					),
+					sample, symbol, count
+			),
+		symbol, count
+		),
+		full_bayesian  = F,
+		do_regression = T,
+		cores = 20,
+		levels = 3
+	)
 
-level_to_plot_inferred_vs_observed(N52_ARMET_T, 1, S = 13)
+expect_gt(
+	N52_ARMET_T$proportions %>% filter(`Cell type category` == "immune_cell") %>% summarise(.value %>% min),
+	0.95
+)
+
+expect_gt(
+	N52_ARMET_T$proportions %>% filter(`Cell type category` == "t_cell") %>% summarise(.value %>% min),
+	0.85
+)
+
+expect_gt(
+	N52_ARMET_T$proportions %>% filter(!converged) %>% nrow,
+	0
+)
+
+N52_ARMET_E =
+	ARMET_tc(
+		spread(
+			select(
+				filter(
+					readRDS("dev/N52.rds"),
+					ct == "E"
+				),
+				sample, symbol, count
+			),
+			symbol, count
+		),
+		full_bayesian  = F,
+		do_regression = T,
+		cores = 20,
+		levels = 1
+	)
+
+expect_gt(
+	N52_ARMET_E$proportions %>% filter(`Cell type category` == "epithelial") %>% summarise(.value %>% min),
+	0.95
+)
+
+expect_gt(
+	N52_ARMET_E$proportions %>% filter(!converged) %>% nrow,
+	0
+)
