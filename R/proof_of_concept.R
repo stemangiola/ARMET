24379444c8dba549b8f99380ed0d4e33ddf64137
stemangiola <mangiolastefano@gmail.com>
2020-04-25 18:40:20 +1000

    proof of concept plotting done

--- R/proof_of_concept.R
+++ R/proof_of_concept.R
@@ -2,6 +2,22 @@ library(tidyverse)
 library(rstan)
 library(tidybayes)
 library(brms)
+
+my_theme = 	
+	theme_bw() +
+	theme(
+		panel.border = element_blank(),
+		axis.line = element_line(),
+		panel.grid.major = element_line(size = 0.2),
+		panel.grid.minor = element_line(size = 0.1),
+		text = element_text(size=12),
+		aspect.ratio=1,
+		#axis.text.x = element_text(angle = 90, hjust = 1),
+		strip.background = element_blank(),
+		axis.title.x  = element_text(margin = margin(t = 10, r = 10, b = 10, l = 10)),
+		axis.title.y  = element_text(margin = margin(t = 10, r = 10, b = 10, l = 10))
+	)
+
 bind <- function(...) cbind(...)
 
 logsumexp <- function (x) {
@@ -15,7 +31,8 @@ softmax <- function (x) {
 
 get_alpha = function(C, slope_value){
 	intercept = 1:C
-	intercept[(C-1):C] = intercept[(C-1):C] * 2
+	#intercept[(C-1):C] = intercept[(C-1):C] * 2
+	#intercept = intercept[(C-1):C] * 2
 	intercept = intercept /5
 	slope = rep(0, C)
 	slope[4] = slope_value
@@ -66,9 +83,8 @@ my_prop_dir_3 =
 	mutate(proportion = count / sum(count)) %>%
 	ungroup()
 
-my_prop_dir_3 %>%
-	ggplot(aes(risk, proportion, color=cell_type)) + 
-	geom_point()
+
+
 
 m = rstan::stan_model("inst/stan/proof_concept.stan")
 fit = 
@@ -88,8 +104,75 @@ fit =
 		cores = 4
 	)
 
-fit %>% gather_draws(alpha_generative[A, C]) %>% filter(A ==2) %>% ggplot(aes(.value, color=factor(C))) + geom_density()
-fit %>% gather_draws(alpha_descriptive[A, C]) %>% filter(A ==2) %>% ggplot(aes(.value, color=factor(C))) + geom_density()
+
+# Plotting
+
+list(
+	my_prop_dir_3 %>%
+		ggplot(aes(risk, rate, color=cell_type)) + 
+		geom_point() +
+		scale_color_brewer(palette="Set1") +
+		xlab("Risk") + 
+		ylab("Cell type abundance rate") +
+		my_theme, 
+
+	my_prop_dir_3 %>%
+		ggplot(aes(risk, count, color=cell_type)) + 
+		geom_point() +
+		scale_color_brewer(palette="Set1") +
+		xlab("Risk") + 
+		ylab("Cell type count") +
+		my_theme,
+	
+	my_prop_dir_3 %>%
+		ggplot(aes(risk, proportion, color=cell_type)) + 
+		geom_point() +
+		scale_color_brewer(palette="Set1") +
+		xlab("Risk") + 
+		ylab("Cell type proportion") +
+		my_theme,
+	
+	fit %>%
+		gather_draws(alpha_generative[A, C]) %>% 
+		filter(A ==2) %>%
+		ungroup() %>%
+		mutate(C = C +1) %>%
+		bind_rows({
+			my_sd = (.) %>% group_by(C) %>% summarise(sd(.value)) %>% pull(2) %>% mean
+			
+			(.) %>%
+				filter(C ==2) %>%
+				mutate(C = rep(1, n())) %>%
+				mutate(.value = rnorm(n(), 0, my_sd))
+			
+		})	 %>% 
+		ggplot(aes(.value, color=factor(C))) + 
+		geom_density() +
+		scale_color_brewer(palette="Set1") +
+		xlab("Slope") + 
+		ylab("Probability density") +
+		my_theme,
+	
+	fit %>%
+		gather_draws(alpha_descriptive[A, C]) %>% 
+		filter(A ==2) %>%
+		ungroup() %>% 
+		ggplot(aes(.value, color=factor(C))) + 
+		geom_density() +
+		scale_color_brewer(palette="Set1") +
+		xlab("Slope") + 
+		ylab("Probability density") +
+		my_theme
+) %>%
+	cowplot::plot_grid(plotlist = .,		 align = "v",		 axis = "b",	 rel_widths = 1) %>%
+	ggsave(
+		filename =	"dev/proof_of_concept_generative_model.pdf",
+		device = "pdf",
+		useDingbats=FALSE,
+		units = c("mm"),
+		width = 280 ,
+		height = 280 
+	)
 
 # # BRMS
 # fit_brms <- brm(
