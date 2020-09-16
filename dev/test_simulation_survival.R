96fa3f6f15364c89e48c6d1eac6377df096d65c8
stemangiola <mangiolastefano@gmail.com>
2020-09-16 20:16:34 +1000

    added new code for simulations

--- dev/test_simulation_survival.R
+++ dev/test_simulation_survival.R
@@ -274,10 +274,12 @@ CI_to_ARMET_one = function(x, CI){
 		mutate(tp = alpha_2 != 0 & significant) 
 }
 
+my_dir = "dev/test_simulation"
+
 # Calculate for for regression
 regression_ARMET =
-	dir("dev/test_simulation/", full.names = TRUE) %>%
-	grep("cibersort", ., invert = T, value = T)%>%
+	dir(my_dir, full.names = TRUE) %>%
+	grep("cibersort|llsr|epic", ., invert = T, value = T)%>%
 	grep("test_noise_survival_regression",., value = T) %>%
 	future_map_dfr(~ {
 			res_regression = readRDS(.x) 
@@ -313,70 +315,138 @@ regression_ARMET =
 	
 	select(-data)
 
-
-
-CI_to_others = function(.data, pvalue){
-	.data %>%
-		imap_dfr(
-			~
-				# Integrate
-				.x$result %>%
-				dplyr::select(.cell_type, estimate, 	p.value) %>%
-				left_join(
-					.x$mix %>% attr("proportions") %>% distinct(`Cell type category`, alpha_2),
-					by = c(".cell_type" = "Cell type category"  )
-				) %>%
-				drop_na  %>%
-				
-				# Calculate
-				mutate(fp = alpha_2 == 0 &	p.value < pvalue) %>%
-				mutate(tp = alpha_2 != 0 &	p.value < pvalue) %>%
-				
-				mutate(run = .y)
-			
-		)
+process_third_party = function(CI, file, input_file, slope){
+	readRDS(file) %>% 
+		
+		# Add truth
+		dplyr::select(.cell_type, estimate, 	p.value) %>%
+		left_join(
+			readRDS(input_file) %>% 
+				attr("proportions") %>% 
+				distinct(`Cell type category`, alpha_2),
+			by = c(".cell_type" = "Cell type category"  )
+		) %>%
+		drop_na  %>%
+		
+		# Calculate
+		mutate(fp = alpha_2 == 0 &	p.value < CI) %>%
+		mutate(tp = alpha_2 != 0 &	p.value < CI)  %>%
+		
+		# Filter out accidental fp because of simplex
+		filter(!(fp & (slope * estimate )<0)) 
 }
 
-regression_cibersort =
-	dir("dev/test_simulation/", full.names = TRUE) %>%
-	grep("cibersort", .,  value = T) %>%
-	enframe(value = "res_cibersort") %>%
-	mutate(CI_df = list(tibble(CI =  rev(1-c( seq(0.0, 0.85, 0.05), seq(0.85, 0.9999, 0.001),seq(0.9999, 0.99999999, 0.000001) ))) )) %>%
-	unnest(CI_df) %>%
-	mutate(roc = future_map2(
-		CI,res_cibersort,
-		~ {
-			x = readRDS(.y) %>% CI_to_others(.x)
-			
-			slope_run = x %>% distinct(alpha_2, run) %>% filter(alpha_2 != 0) %>% rename(slope_run = alpha_2)
-			
-			
-			x %>%
-				left_join(slope_run, by="run") %>%
-				mutate(abs_slope_run = abs(slope_run)) %>%
-				
-				
-				# Filter out accidental fp because of simplex
-				filter(!(fp & (slope_run * estimate )<0))  %>%
-			
-				nest(data = -abs_slope_run) %>%
-				mutate(real_negative = map_dbl(data, ~ .x %>% filter(alpha_2==0) %>% nrow)) %>%
-				mutate(FP = map_dbl(data, ~ .x %>% filter(fp) %>% nrow)) %>%
-				mutate(real_positive = map_dbl(data, ~ .x %>% filter(alpha_2!=0) %>% nrow )) %>%
-				mutate(TP = map_dbl(data, ~ .x %>% filter(tp) %>% nrow) ) %>%
-				mutate(TP_rate = TP/real_positive, FP_rate = FP/real_negative) %>%
-				
-				select(-data)
-			
-			# x %>% filter(alpha_2 != 0) %>% group_by(alpha_2) %>%
-			# 	summarise(fnr = sum(fn) / n()) %>%
-			# 	mutate(
-			# 		FP_rate =
-			# 			x %>% filter(alpha_2 == 0) %>% select(-alpha_2) %>%
-			# 			summarise(FP_rate = sum(fp) / n()) %>% pull(1)
-			# 	)
-		}
-	))
+process_ARMET = function(CI, file, input_file, slope){
+	readRDS(file) %$%
+		proportions %>% 
+		#get_CI(CI) %>%
+		
+		mutate(.value_2 = map_dbl(
+			draws_cens,
+			~ .x %>% 
+				filter(A == 2) %>%
+				pull(.value ) %>%
+				mean
+		)) %>%
+		
+		mutate(prob_non_0_2 = map_dbl(
+			draws_cens,
+			~ .x %>% 
+				filter(A == 2) %>% 
+				ARMET:::draws_to_prob_non_zero()
+		)) %>%
+		
+		# Add truth
+		dplyr::select(`Cell type category`, .value_2, prob_non_0_2) %>%
+		left_join(
+			readRDS(input_file) %>% 
+				attr("proportions") %>% 
+				distinct(`Cell type category`, alpha_2),
+			by = c("Cell type category"  )
+		) %>%
+		drop_na  %>%
+		
+		# Calculate
+		mutate(fp = (alpha_2 == 0) &	abs(prob_non_0_2) > CI) %>%
+		mutate(tp = (alpha_2 != 0) &	abs(prob_non_0_2) > CI) %>%
+		
+		# Filter out accidental fp because of simplex
+		filter(!(fp & (slope * .value_2 )<0))  
+	
+}
+
+roc_df = 
+	
+	dir(my_dir, full.names = TRUE) %>%
+	enframe(value = "file", name = NULL) %>% 
+	
+	# format
+	separate(file, as.character(1:7), sep = "__", remove = F) %>% 
+	gather(which, value, -file) %>% 
+	mutate(value = gsub("dev/test_simulation/", "", value)) %>% 
+	mutate(value = gsub(".rds", "", value, fixed = T))  %>% 
+	tidyr::extract(value, c("prefix", "value"), "([a-zA-z]+_)?(.+)") %>% 
+	mutate(prefix = if_else(prefix == "", "in_out", gsub("_", "", prefix))) %>%
+	select(-which) %>%
+	spread(prefix, value) %>%
+	nest(data = -c(foreignProp, run, S, slope, whichChanging)) %>%
+	mutate(data = map(
+		data,
+		~.x %>% 
+			filter(in_out=="output") %>% 
+			mutate(input_file = 
+						 	.x %>% 
+						 	filter(in_out=="input") %>% 
+						 	pull(file)
+						)
+	)) %>%
+	unnest(data) %>%
+	select(-`<NA>`) %>%
+	mutate(slope = as.numeric(slope)) %>%
+	
+	# Add CI interval
+	mutate(
+		CI = 
+			if_else(
+				method=="ARMET",
+				list(c( seq(0.0, 0.85, 0.05), seq(0.85, 0.9999, 0.001))),
+				list(rev(1-c( seq(0.0, 0.85, 0.05), seq(0.85, 0.9999, 0.001),seq(0.9999, 0.99999999, 0.000001) )))
+			)
+	) %>%
+	unnest(CI) %>%
+	
+	# Calculate
+	slice(1) %>%
+	mutate(fp_tp = future_pmap(
+		list(CI,file, input_file, slope, method),
+		~ ..5 %>% purrr::when(
+			(.) == "ARMET" ~ process_ARMET(..1, ..2, ..3, ..4),
+			~ process_third_party(..1, ..2, ..3, ..4)
+
+	))) %>%
+	unnest(fp_tp) %>%
+	
+	# Calculate ROC
+	mutate(abs_slope = abs(slope)) %>%
+	nest(data = -c(foreignProp, S, abs_slope, method, CI)) %>%
+	mutate(real_negative = map_dbl(data, ~ .x %>% filter(alpha_2==0) %>% nrow)) %>%
+	mutate(FP = map_dbl(data, ~ .x %>% filter(fp) %>% nrow)) %>%
+	mutate(real_positive = map_dbl(data, ~ .x %>% filter(alpha_2!=0) %>% nrow )) %>%
+	mutate(TP = map_dbl(data, ~ .x %>% filter(tp) %>% nrow) ) %>%
+	mutate(TP_rate = TP/real_positive, FP_rate = FP/real_negative) %>%
+	
+	select(-data)
+
+
+
+roc_df %>%
+	arrange(FP_rate, TP_rate) %>%
+	ggplot(aes(x=FP_rate, y=TP_rate, color=method)) +
+	geom_abline(intercept = 0, slope = 1, linetype="dotted", color="grey") +
+	geom_line() +
+	scale_color_brewer(palette = "Set1") +
+	facet_wrap(~ abs_slope + foreignProp + S, nrow = 1) +
+	coord_cartesian(xlim=c(0,0.08), ylim=c(0,1))
 
 # merge results
 merged = 
